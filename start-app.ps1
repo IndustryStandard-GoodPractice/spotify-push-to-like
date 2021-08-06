f0ffab12ac6d4cc3ea5f522db6941eef74538aa8
Durush <Demonto123@gmail.com>
2021-08-06 00:06:08 -0400

    Update start-app.ps1

--- start-app.ps1
+++ start-app.ps1
@@ -1,7 +1,7 @@
-npm install
-Start-Process node app.js
+npm install --prefix $PSScriptRoot
+Start-Process node $PSScriptRoot/app.js
 [system.Diagnostics.Process]::Start("chrome","http://localhost:8888/login")
 
 $LISTENER_PATH = './pyth-keybind-listener'
-pip install -r $LISTENER_PATH/requirements.txt --user
-Start-Process python $LISTENER_PATH/listener.py
+pip install -r $PSScriptRoot/$LISTENER_PATH/requirements.txt --user
+Start-Process python $PSScriptRoot/$LISTENER_PATH/listener.py
