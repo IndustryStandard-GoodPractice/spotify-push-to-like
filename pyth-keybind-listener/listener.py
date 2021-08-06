ff4d9319025c180ffb93bfbcb9e65e77279a9ff4
Durush <Demonto123@gmail.com>
2021-08-05 23:08:38 -0400

    final steps

---
+++ pyth-keybind-listener/listener.py
@@ -0,0 +1,79 @@
+# Imports
+import win32con
+import ctypes, ctypes.wintypes
+import sys
+import requests
+
+#
+# Functions
+#
+def dispatch_hotkey(msg):
+    #mod = msg.lParam & 0b1111111111111111
+    #key = msg.lParam >> 16
+    #bit = bin(msg.lParam)[2:]
+    #print("\n*** Received hotkey message (wParam: %d, lParam: %d)" % (msg.wParam, msg.lParam))
+    #print("lParam bitmap: %s" % bit)
+    #print("lParam low-word (modifier): %d, high-word (key): %d" % (mod, key))
+    print("-> Hotkey detected!!!\n")
+    r = requests.get("http://localhost:8888/like-current-track")
+
+#
+# Main
+#
+
+# Build translation maps (virtual key codes / modifiers to string)
+# Note: exec() is a hack and should not be used in real programs!!
+print("\n*** Building translation maps")
+mods = {}
+keys = {}
+for item in dir(win32con):
+    if item.startswith("MOD_"):
+        exec("mods[item] = win32con." + item)
+        exec("mods[win32con." + item + "] = '" + item + "'")
+    if item.startswith("VK_"):
+        exec("keys[item] = win32con." + item)
+        exec("keys[win32con." + item + "] = '" + item + "'")
+
+# Process command line
+print("\n*** Processing command line")
+
+mod = "MOD_WIN"
+key = "VK_ESCAPE"
+for param in sys.argv:
+    if param.startswith("MOD_"):
+        if param in mods: mod = param
+        else: print("\nInvalid modifier specified (%s). Using default.\n-> Use '--list-mods' for a list of valid modifiers." % param)
+    if param.startswith("VK_"):
+        if param in keys: key = param
+        else: print("\nInvalid key specified (%s). Using default.\n-> Use '--list-keys' for a list of valid keys." % param)
+
+if "--list-mods" in sys.argv:
+    print("\nAvailable modifiers:")
+    for item in dir(win32con):
+        if item.startswith("MOD_"): sys.stdout.write(item + ", ")
+    print("\b\b ")
+
+if "--list-keys" in sys.argv:
+    print("\nAvailable keys:")
+    for item in dir(win32con):
+        if item.startswith("VK_"): sys.stdout.write(item + ", ")
+    print("\b\b ")
+
+# Register hotkey
+print("\n*** Registering global hotkey (modifier: %s, key: %s)" % (mod, key))
+if ctypes.windll.user32.RegisterHotKey(None, 1, win32con.MOD_CONTROL, ord('L')) != 0:
+
+    # Wait for hotkey to be triggered
+    print("\n*** Waiting for hotkey message...")
+    try:
+        msg = ctypes.wintypes.MSG()
+        while ctypes.windll.user32.GetMessageA(ctypes.byref(msg), None, 0, 0) != 0:
+            if msg.message == win32con.WM_HOTKEY:
+                dispatch_hotkey(msg)
+            
+            ctypes.windll.user32.TranslateMessage(ctypes.byref(msg))
+            ctypes.windll.user32.DispatchMessageA(ctypes.byref(msg))
+
+    # Unregister hotkey
+    finally:
+        ctypes.windll.user32.UnregisterHotKey(None, 1)
--- start-app.ps1
+++ start-app.ps1
@@ -1,2 +1,6 @@
+npm install
 Start-Process node app.js
+
+pip install -r './pyth-keybind-listener/requirements.txt'
+Start-Process python './pyth-keybind-listener/listener.py'
 [system.Diagnostics.Process]::Start("chrome","http://localhost:8888/login")
