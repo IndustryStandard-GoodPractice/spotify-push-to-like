ac95063abb95cf19bd812cfc3c5442076b986b3e
Durush <47361958+Durush@users.noreply.github.com>
2021-08-06 12:20:08 -0400

    hide terminals on app start

--- start-app.ps1
+++ start-app.ps1
@@ -1,9 +1,11 @@
 cd $PSScriptRoot
 npm install
-Start-Process node $PSScriptRoot/app.js -WindowStyle Minimized
+Start-Process node $PSScriptRoot/app.js -WindowStyle hidden
 [system.Diagnostics.Process]::Start("chrome","http://localhost:8888/login")
 
 $LISTENER_PATH = './pyth-keybind-listener'
 $env:PYTHONIOENCODING = "UTF-8"
 pip install -r $PSScriptRoot/$LISTENER_PATH/requirements.txt --user
-Start-Process python $PSScriptRoot/$LISTENER_PATH/listener.py -WindowStyle Minimized
+$PYTH_PROC = Start-Process python $PSScriptRoot/$LISTENER_PATH/listener.py -WindowStyle hidden -PassThru
+$PYTH_PROC | Export-Clixml -Path (Join-Path $ENV:temp 'processhandle.xml')
+$PYTH_PROC
--- stop-node-app.ps1
+++ stop-node-app.ps1
@@ -2,3 +2,6 @@ $NODE_APP = Get-Process -Id (Get-NetTCPConnection -LocalPort 8888).OwningProcess
 $NODE_ID = $NODE_APP.Id
 
 Stop-Process -Id $NODE_ID
+
+$PYTH_PROC = Import-Clixml -Path (Join-Path $ENV:temp 'processhandle.xml')
+$PYTH_PROC | Stop-Process
