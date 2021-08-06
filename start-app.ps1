83ea9c89e84f06add0416203080f6d4b7808ba82
Durush <47361958+Durush@users.noreply.github.com>
2021-08-05 23:33:48 -0400

    small fixes

--- start-app.ps1
+++ start-app.ps1
@@ -3,5 +3,5 @@ Start-Process node app.js
 [system.Diagnostics.Process]::Start("chrome","http://localhost:8888/login")
 
 $LISTENER_PATH = './pyth-keybind-listener'
-pip install -r $LISTENER_PATH/requirements.txt
+pip install -r $LISTENER_PATH/requirements.txt --user
 Start-Process python $LISTENER_PATH/listener.py
